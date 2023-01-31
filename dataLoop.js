2f2077516cd018e9842b03621d6b47140ce8d00a
Parker <122317767+parkerstafforddev@users.noreply.github.com>
2023-01-31 11:11:35 -0800

    Add files via upload

---
+++ dataLoop.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+export default function DataLoop({ todo, checkBox }) {
+    function changeCheckBox() {
+        checkBox(todo.id)
+    }
+
+    return (
+        <div className="flex-column">
+            <label className="flex-row">
+                <input type="checkbox" checked={todo.isComplete} onChange={ changeCheckBox } />
+                { todo.name }
+            </label>
+        </div>
+    )
+}
